d74ebc447c9747951a3ab4b71c2b76fcb3db9562
Francis Lagares <francis.lagares@gmail.com>
2020-06-30 14:00:50 +0200

    feat: store jwt secret with kubernetes

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -34,6 +34,10 @@ app.all('*', async (req, res) => {
 app.use(errorHandler);
 
 const start = async () => {
+  if (!process.env.JWT_KEY) {
+    console.log('JWT_KEY must be defined');
+  }
+
   try {
     await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
       useNewUrlParser: true,
