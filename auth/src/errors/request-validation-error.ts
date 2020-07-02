cbbb66b34cfe6dc5828e28993d40d06548dd0899
Francis Lagares <francis.lagares@gmail.com>
2020-07-02 18:04:25 +0200

    fix: change statusCode to 400

--- auth/src/errors/request-validation-error.ts
+++ auth/src/errors/request-validation-error.ts
@@ -2,7 +2,7 @@ import { ValidationError } from 'express-validator';
 import { CustomError } from './custom-error';
 
 export class RequestValidationError extends CustomError {
-  statusCode = 404;
+  statusCode = 400;
 
   constructor(public errors: ValidationError[]) {
     super('Invalid request parameters');
