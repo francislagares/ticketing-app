0d8b293a8ed99498992ede53a07de6070d3ec9a1
Francis Lagares <francis.lagares@gmail.com>
2020-07-05 19:54:22 +0200

    feat: create auth  pages
    This commit includes signup, signin and signout functionalities.
    As the main focus of this project is to implement microservices
    it wasn't a priority to implement a reusable form component.

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,7 +1,11 @@
 import buildClient from '../api/build-client';
 
 const LandingPage = ({ currentUser }) => {
-  return currentUser ? <h1>You are sign in</h1> : <h1>You are NOT sign in</h1>;
+  return currentUser ? (
+    <h1>You are signed in</h1>
+  ) : (
+    <h1>You are NOT signed in</h1>
+  );
 };
 
 LandingPage.getInitialProps = async (context) => {
