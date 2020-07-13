db28120d700e7919d0f93f041a1ae816fed729f3
Francis Lagares <francis.lagares@gmail.com>
2020-07-13 20:03:47 +0200

    feat: implement ticket routes

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -3,7 +3,16 @@ import 'express-async-errors';
 import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
 
-import { errorHandler, NotFoundError } from '@flagares-ticketing/common';
+import {
+  errorHandler,
+  NotFoundError,
+  currentUser,
+} from '@flagares-ticketing/common';
+
+import { indexTicketRouter } from './routes/index';
+import { createTicketRouter } from './routes/new';
+import { showTicketRouter } from './routes/show';
+import { updateTicketRouter } from './routes/update';
 
 const app = express();
 
@@ -15,6 +24,12 @@ app.use(
     secure: process.env.NODE_ENV !== 'test',
   })
 );
+
+app.use(currentUser);
+app.use(createTicketRouter);
+app.use(showTicketRouter);
+app.use(indexTicketRouter);
+app.use(updateTicketRouter);
 
 app.all('*', async (req, res) => {
   throw new NotFoundError();
