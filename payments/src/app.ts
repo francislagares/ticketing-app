e6f4611b941935fd7c13e28b1ac64d0e9c094d02
Francis Lagares <francis.lagares@gmail.com>
2020-08-04 15:12:01 +0200

    feat(payments): implement route for new payment

--- payments/src/app.ts
+++ payments/src/app.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
+import { createChargeRouter } from './routes/new';
 
 import {
   errorHandler,
@@ -21,6 +22,7 @@ app.use(
 );
 
 app.use(currentUser);
+app.use(createChargeRouter);
 
 app.all('*', async (req, res) => {
   throw new NotFoundError();
---
+++ payments/src/routes/new.ts
@@ -0,0 +1,49 @@
+import express, { Request, Response } from 'express';
+import { body } from 'express-validator';
+import { Order } from '../models/order';
+import { Stripe } from 'stripe';
+import {
+  requireAuth,
+  validateRequest,
+  BadRequestError,
+  NotFoundError,
+  NotAuthorizedError,
+  OrderStatus,
+} from '@flagares-ticketing/common';
+import { stripe } from '../stripe';
+
+const router = express.Router();
+
+router.post(
+  '/api/payments',
+  requireAuth,
+  [body('token').not().isEmpty(), body('orderId').not().isEmpty()],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const { token, orderId } = req.body;
+
+    const order = await Order.findById(orderId);
+
+    if (!order) {
+      throw new NotFoundError();
+    }
+
+    if (order.userId !== req.currentUser!.id) {
+      throw new NotAuthorizedError();
+    }
+
+    if (order.status === OrderStatus.Cancelled) {
+      throw new BadRequestError('Cannot pay for an cancelled order');
+    }
+
+    await stripe.charges.create({
+      currency: 'usd',
+      amount: order.price * 100,
+      source: token,
+    });
+
+    res.send({ success: true });
+  }
+);
+
+export { router as createChargeRouter };
