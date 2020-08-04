f16f35a72b17bfb7cddc31f240082a1660affd22
Francis Lagares <francis.lagares@gmail.com>
2020-08-04 12:09:58 +0200

    feat(payments): implement order payments listener event

---
+++ payments/src/events/listeners/order-cancelled-listener.ts
@@ -0,0 +1,27 @@
+import {
+  OrderCancelledEvent,
+  Subjects,
+  Listener,
+  OrderStatus,
+} from '@flagares-ticketing/common';
+import { queueGroupName } from './queue-group-name';
+import { Message } from 'node-nats-streaming';
+import { Order } from '../../models/order';
+
+export class OrderCancelledListener extends Listener<OrderCancelledEvent> {
+  readonly subject: Subjects.OrderCancelled = Subjects.OrderCancelled;
+  queueGroupName = queueGroupName;
+
+  async onMessage(data: OrderCancelledEvent['data'], msg: Message) {
+    const order = await Order.findByEvent(data);
+
+    if (!order) {
+      throw new Error('Order not found');
+    }
+
+    order.set({ status: OrderStatus.Cancelled });
+    await order.save();
+
+    msg.ack();
+  }
+}
---
+++ payments/src/events/listeners/order-created-listener.ts
@@ -0,0 +1,26 @@
+import { Message } from 'node-nats-streaming';
+import {
+  Listener,
+  OrderCreatedEvent,
+  Subjects,
+} from '@flagares-ticketing/common';
+import { queueGroupName } from './queue-group-name';
+import { Order } from '../../models/order';
+
+export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
+  readonly subject: Subjects.OrderCreated = Subjects.OrderCreated;
+  queueGroupName = queueGroupName;
+
+  async onMessage(data: OrderCreatedEvent['data'], msg: Message) {
+    const order = Order.build({
+      id: data.id,
+      price: data.ticket.price,
+      status: data.status,
+      userId: data.userId,
+      version: data.version,
+    });
+    await order.save();
+
+    msg.ack();
+  }
+}
