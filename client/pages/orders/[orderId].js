2cb70282c8d1eb3f1abbee1c1cd2f6c55e258f48
Francis Lagares <francis.lagares@gmail.com>
2020-08-05 20:18:48 +0200

    feat(client): integrate stripe payment form

---
+++ client/pages/orders/[orderId].js
@@ -0,0 +1,55 @@
+import { useEffect, useState } from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+import useRequest from '../../hooks/use-request';
+
+const OrderShow = ({ order, currentUser }) => {
+  const [timeLeft, setTimeLeft] = useState(0);
+  const { doRequest, errors } = useRequest({
+    url: '/api/payments',
+    method: 'post',
+    body: {
+      orderId: order.id,
+    },
+    onSuccess: (payment) => console.log(payment),
+  });
+
+  useEffect(() => {
+    const findTimeLeft = () => {
+      const msLeft = new Date(order.expiresAt) - new Date();
+      setTimeLeft(Math.round(msLeft / 1000));
+    };
+
+    findTimeLeft();
+    const timerId = setInterval(findTimeLeft, 1000);
+
+    return () => {
+      clearInterval(timerId);
+    };
+  }, [order]);
+
+  if (timeLeft < 0) {
+    return <div>Order Expired</div>;
+  }
+
+  return (
+    <div>
+      Time left to pay: {timeLeft} seconds
+      <StripeCheckout
+        token={({ id }) => doRequest({ token: id })}
+        stripeKey='pk_test_51HCPhEG0sixEZsUe9Z60h3fh2BRjjrYZMHQnsobUvqHSc6suQ52Usu5RtD12oAPmx4b93fP6WoRSU3Zm94KQpB7I006K6tkZ8p'
+        amount={order.ticket.price * 100}
+        email={currentUser.email}
+      />
+      {errors}
+    </div>
+  );
+};
+
+OrderShow.getInitialProps = async (context, client) => {
+  const { orderId } = context.query;
+  const { data } = await client.get(`/api/orders/${orderId}`);
+
+  return { order: data };
+};
+
+export default OrderShow;
--- client/pages/tickets/[ticketId].js
+++ client/pages/tickets/[ticketId].js
@@ -1,5 +1,5 @@
+import Router from 'next/router';
 import useRequest from '../../hooks/use-request';
-import { useReducer } from 'react';
 
 const TicketShow = ({ ticket }) => {
   const { doRequest, errors } = useRequest({
@@ -8,14 +8,15 @@ const TicketShow = ({ ticket }) => {
     body: {
       ticketId: ticket.id,
     },
-    onSuccess: (order) => console.log(order),
+    onSuccess: (order) =>
+      Router.push('/orders/[orderId]', `/orders/${order.id}`),
   });
   return (
     <div>
       <h1>{ticket.title}</h1>
       <h4>Price: {ticket.price}</h4>
       {errors}
-      <button onClick={doRequest} className='btn btn-primary'>
+      <button onClick={() => doRequest()} className='btn btn-primary'>
         Purchase
       </button>
     </div>
